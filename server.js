c6535cb5d84818667f5d0cdaf28d16a87af28af0
shanizlo <shanizlotnik@gmail.com>
2020-03-28 23:52:40 +0300

    done database integration for profile/id, signin and register

--- server.js
+++ server.js
@@ -53,28 +53,29 @@ app.post('/signin', (req, res) => {
 
 app.post('/register', (req, res) => {
 	const { email, name, password } = req.body;
-	db('users').insert({
-		email: email,
-		name: name,
-		joined: new Date()
-	}).then(console.log)
-	res.json(database.users[database.users.length-1]);
-
-})
+	db('users')
+		.returning('*')
+		.insert({
+			email: email,
+			name: name,
+			joined: new Date()
+	}).then(user => {
+		res.json(user[0]);
+	})
+	.catch(err => res.status(400).json('unable to register! '));
+});
 
 app.get('/profile/:id', (req, res) => {
 	const { id } = req.params;
-	let found = false;
-	database.users.forEach(user => {
-		if (user.id === id) {
-			found = true;
-			res.json(user);	
-		}		
-	})
-	if (!found) {
-		res.status(400).json('user not found');
-	}
-})
+	db.select('*').from('users').where({ id: id })
+		.then(user => {
+			if (user.length) {
+				res.json(user[0]);
+			} else {
+				res.status(400).json('error getting user');
+			}
+		});
+});
 
 app.put('/image', (req, res) => {
 	const { id } = req.body;
