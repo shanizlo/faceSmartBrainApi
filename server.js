85b008f02c0cbb5aa4e4acb9c2924b82e83526dc
shanizlo <shanizlotnik@gmail.com>
2020-04-10 21:44:07 +0300

    changed host

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : 'postgresql-amorphous-07328',
-    user : 'shaniz',
-    password : '',
-    database : 'smart-brain'
+    host : process.env.DATABASE_URL,
+    ssl: true
   }
 });
 
