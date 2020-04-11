044f0ca52554f5138548de0addea52dd3b9dbbf4
shanizlo <shanizlotnik@gmail.com>
2020-04-11 21:27:36 +0300

    commented out db connection

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
 
-const db = knex({
-  client: 'pg',
-  connection: {
-    connectionString : process.env.DATABASE_URL,
-    ssl: true,
-  }
-});
+// const db = knex({
+//   client: 'pg',
+//   connection: {
+//     connectionString : process.env.DATABASE_URL,
+//     ssl: true,
+//   }
+// });
 
 const app = express()
 app.use(cors());
